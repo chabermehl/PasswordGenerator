5309bb77195aebdbdf5bc68e9ee67ebde7dc0734
chabermehl <clp.habermehl@gmail.com>
2019-06-30 22:04:30 -0600

    Added arguments to the `generate` function
    Added word count and letter length arguments to the function.

--- password_generator.py
+++ password_generator.py
@@ -2,7 +2,7 @@ import json
 import random
 
 
-def generate():
+def generate(num_words, num_letters):
     file = open("words_dictionary.json", "r")
 
     words = json.load(file)
@@ -11,10 +11,10 @@ def generate():
     password = ""
 
     # change the counter max to be any number, just depends on how long you want you passwords
-    while counter <= 1:
+    while counter <= num_words:
         word = random.choice(list(words))
         # word length can be easily changed to make different passwords
-        if(len(word) == 7):
+        if(len(word) == num_letters):
             password += word
             counter += 1
 
@@ -23,4 +23,4 @@ def generate():
     print(password + str(random_int))
 
 
-generate()
+generate(1, 7)
