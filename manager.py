13a326a5f699dfb9d0d3a77362c378bdd7e1e86d
Damon <mdamon.connolly@gmail.com>
2019-07-22 20:45:54 +0100

    Conformed to PEP8 standard.
    This commit adds the manager in its most basic form. It's designed as an interface so that it can be used from the main.py file and ready for a GUI.
    
    I've also used pycodestyles to conform the script to the PEP8 standard.

--- manager.py
+++ manager.py
@@ -9,53 +9,47 @@ file to sync over google drive etc, and also to keep passwords logically
 partitioned on disk.
 '''
 
+
 class manager():
 
-	def __init__(self):
+    def __init__(self):
 
-		self.entries = {}
+        self.path = '.\\'
+        self.entries = {}
 
+    def load_vault(self):
 
-	'''
-	Load JSON file containing all dict entries
-	This will later be encrypted!
-	'''
-	def load_vault(self, path):
+        try:
+            with open(self.path, 'r') as file:
+                self.entries = json.load(file)
+                print(self.entries)
+        except ImportError:
 
-		try:
-			with open(path, 'r') as file:
-				self.entries = json.load(file)
-				print(self.entries)
-		except:
+            print('File does not exist...')
 
-			print("File does not exist...\nCreate New?")
+    def save_vault(self):
 
-	def save_vault(self, path):
-		
-		with open(path, 'w', encoding='utf-8') as file:
-			json.dump(self.entries, file, ensure_ascii=False, indent=4)
+        with open(self.path, 'w', encoding='utf-8') as file:
+            json.dump(self.entries, file, ensure_ascii=False, indent=4)
 
+    def create_entry(self, title, username, password=""):
 
+        '''TODO: If no password, use generator to create one'''
+        self.entries.update({title: [username, password]})
 
-	def create_entry(self, title, username, password=""):
-		'''TODO: If no password, use generator to create one'''
-		self.entries.update({title: [username, password]})
+    def edit_entry(self, title, username="", password=""):
 
-	
-	def edit_entry(self, title, username="", password=""):
+        if len(username) > 1:
+            self.entries[title][0] = username
 
-		if len(username) > 1:
-			self.entries[title][0] = username
-		
-		if len(password) > 1:
-			self.entries[title][1] = password
+        if len(password) > 1:
+            self.entries[title][1] = password
 
+    def delete_entry(self, title):
+        self.entries.pop(title, None)
 
-	def delete_entry(self, title):
+    def get_all_entries(self):
+        return self.entries
 
-		self.entries.pop(title, None)
-
-
-	def get_all_entries(self):
-
-		return self.entries
+    def set_path(self, path):
+        self.path = path
